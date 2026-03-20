6fc9ecd31a26470fd45655d4b0742ba3c8db051c
thinurahk <thinuraedu@gmail.com>
2026-03-20 12:51:08 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: {
-    appIsrStatus: false,
-    buildActivityPosition: "bottom-right",
-    buildActivity: false,
-  },
+  devIndicators: false,
 };
 
 export default nextConfig;
